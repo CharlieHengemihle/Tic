86993b5bdbf54695d0d606e46191892acfaa5a81
Charlie Hengemihle <charliehengemihle@gmail.com>
2023-01-26 16:56:50 -0700

    Step one and 2, getting the state and framework in

---
+++ src/context/GameContext.js
@@ -0,0 +1,30 @@
+const { createContext, useState, useContext } = require('react');
+
+const GameContext = createContext();
+
+const GameProvider = ({ children }) => {
+  const [board, setBoard] = useState([]);
+  const [player, setPlayer] = useState('X');
+  const [active, setActive] = useState(true);
+  const [message, setMessage] = useState('Big, beautiful Bodacious the Bull');
+
+  return (
+    <GameContext.Provider
+      value={{ board, setBoard, player, setPlayer, active, setActive, message, setMessage }}
+    >
+      {children}
+    </GameContext.Provider>
+  );
+};
+
+const useGameContext = () => {
+  const context = useContext(GameContext);
+
+  if (context === undefined) {
+    throw new Error('Nope');
+  }
+
+  return context;
+};
+
+export { GameProvider, useGameContext };
